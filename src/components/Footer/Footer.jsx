ccfbd41882a3483298d1f8cf33008c107c5bd8e6
phendges7 <uk_hendges@outlook.com>
2025-05-29 21:02:43 +0100

    adjusting CSS and filtering collection
    fix: fixing collection filter
    fix: adjust CSS

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 const Footer = () => {
   return (
     <footer className="footer">
+      <p>Este site é construído por pessoas como você. </p>
+      <p>Cada contribuição importa. 💙</p>
       <p>© 2025 - TUDO NOSSO, NADA DELES.</p>
     </footer>
   );
